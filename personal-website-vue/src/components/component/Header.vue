e13ae32c2d9c8fff0fdf1ef18349dedfeea7d623
Charijoy Cempron <guineverbaroque17@gmail.com>
2025-03-05 19:23:03 +0000

    try

---
+++ personal-website-vue/src/components/component/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header :class="{ scrolled: isScrolled }">
+    <div class="logo">
+      <h1></h1>
+    </div>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/#skills">Skills</router-link></li>
+        <li><router-link to="/#hobbies">Interests</router-link></li>
+        <li><router-link to="/#resources">Resources</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      isScrolled: false
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      this.isScrolled = window.scrollY > 50;
+    }
+  }
+}
+</script>
---
+++ personal-website-vue/src/components/component/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="hero">
+    <div class="hero-content">
+      <img src="@/assets/images/sit.jpg" alt="Profile Photo" class="profile-image">
+      <div class="hero-text">
+        <h2>Charijoy Cempron</h2>
+        <p class="subtitle">IT Student & Web Enthusiast</p>
+        <p>Cha/ri, Kakay, Acerola</p>
+        <p>Metro Manila, Philippines</p>
+        <div class="social-links">
+          <a href="https://www.linkedin.com/in/charijoy-cempron-0b6950281/" target="_blank">
+            <i class="fab fa-linkedin"></i> LinkedIn
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+}
+</script>
